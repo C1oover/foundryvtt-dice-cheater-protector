5482b295957f21aec2b65a5cf80d2417ed8f8076
C1oover <112273679+C1oover@users.noreply.github.com>
2024-06-30 09:36:36 +0200

    Change to Mersenne Twister

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,16 @@
 import { Settings } from './settings.js';
-// Generates random number using a cryptographic method
-function getCryptoRandom() {
-	let buffer = new ArrayBuffer(8);
-	let ints = new Int8Array(buffer);
-	window.crypto.getRandomValues(ints);
-	ints[7] = 63;
-	ints[6] |= 0xf0;
-	return new DataView(buffer).getFloat64(0, true) - 1;
-}
 // Generates random number with fudge factor based on the current settings
 function getRandom() {
-	let r = getCryptoRandom();
+	let r = MersenneTwister.random();
 	switch (Settings.fudgeValue) {
 	case "MINIMAL":
-		return 0.001;
+		return 0.9999;
 	case "MAXIMAL":
-		return 1;
+		return 0.0001;
 	case "HIGH":
-		return r < 0.49 && getCryptoRandom() >= 0.5 ? r * 2 : r;
+		return r > 0.49 && MersenneTwister.random() >= 0.5 ? r / 2 : r;
 	case "LOW":
-		return r > 0.49 && getCryptoRandom() >= 0.5 ? r / 2 : r;
+		return r < 0.49 && MersenneTwister.random() >= 0.5 ? r * 2 : r;
 	case "CUSTOM":
 		if (Settings.exactFudgeValue / 100 <= 1){
 			return Settings.exactFudgeValue / 100;
@@ -33,7 +24,7 @@ Hooks.once('init', () => {
 });
 Hooks.once('ready', () => {
 	// Use getRandom() or getCryptoRandom() based on settings
-	CONFIG.Dice.randomUniform = (Settings.getEnableFudgeDice()) ? getRandom : getCryptoRandom;
+	CONFIG.Dice.randomUniform = (Settings.getEnableFudgeDice()) ? getRandom : MersenneTwister.random();
 	// Freeze the Dice class for players to avoid modification (harder to cheat)
 	if (!Settings.getOnlyForPlayer() || !game.user.hasRole("TRUSTED",1)) {
 		Object.freeze(CONFIG.Dice);
